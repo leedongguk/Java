8ec1ad18c11a38240f2c36fad084d8dff06eeea0
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-08-26 02:28:46 +0900

    Create [2739]구구단.java

---
+++ Level3/[2739]구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+	public class Main {
+	    public static void main(String[] args) {
+
+	    	Scanner in = new Scanner(System.in);
+	    	
+	    	int A = in.nextInt();
+	    	int i=1;
+	    	
+      while(i<=9) {
+    	  
+    	  System.out.println(A+" * "+i+" = " + A*i );
+    	  i++;
+    	  
+      }
+	    	
+	    }
+	}
