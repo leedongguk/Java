9cc5558251a8b50b3fcf939e466d358647c40dfb
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-08-26 02:32:17 +0900

    Create [10950]A+B-3.java

---
+++ Level3/[10950]A+B-3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+	public class Main {
+	    public static void main(String[] args) {
+
+	    	Scanner in = new Scanner(System.in);
+	    	
+	    	int A = in.nextInt();
+	    	int i=1;
+	    	
+      while(i<=A) {
+    	  
+	      int B = in.nextInt();
+	      int C = in.nextInt();
+    	  System.out.println(B + C);
+    	  i++;
+    	  
+      }
+	    	
+	    }
+	}
