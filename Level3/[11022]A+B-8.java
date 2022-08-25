f5d91345d7eadf08f203180ac0d48f5c2ecae097
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-08-26 02:33:56 +0900

    Create [11022]A+B-8.java

---
+++ Level3/[11022]A+B-8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+	public class Main {
+	    public static void main(String[] args) {
+
+	    	Scanner in = new Scanner(System.in);
+	    	
+	    	int A = in.nextInt();
+
+	    	
+	    	for(int i = 1; i<=A;i++) {
+	    		
+		    	int B = in.nextInt();
+		    	int C = in.nextInt();
+		    	
+		    	System.out.println("Case #"+i+": " + B + " + "+ C + " = "+ (B + C));
+	    		
+	    	}
+	    }
+	}
